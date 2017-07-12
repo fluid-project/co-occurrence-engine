e3c3434c4eac12a083773b7cb52bc19fe25e727a
Simon Bates <simon@bitstructures.com>
2017-07-12 14:53:47 -0300

    GPII-2415: Add runner for nexusWithCoOccurrenceEngine

---
+++ nexusWithCoOccurrenceEngine.js
@@ -0,0 +1,22 @@
+/*
+Copyright 2017 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://raw.githubusercontent.com/GPII/nexus/master/LICENSE.txt
+*/
+
+/* eslint-env node */
+
+"use strict";
+
+var kettle = require("kettle");
+
+require("./index.js");
+
+kettle.config.loadConfig({
+    configName: kettle.config.getConfigName("gpii.nexusWithCoOccurrenceEngine.config"),
+    configPath: kettle.config.getConfigPath("%gpii-co-occurrence-engine/configs")
+});
